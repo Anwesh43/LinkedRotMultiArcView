3ae3935c6a1d311f1c19c70f24dc2b8766babf96
Anwesh43 <anweshthecool0@gmail.com>
2021-05-28 16:29:17 +0530

    adding RMANode class

--- rotmultiarcview/src/main/java/com/example/rotmultiarcview/RotMultiArcView.kt
+++ rotmultiarcview/src/main/java/com/example/rotmultiarcview/RotMultiArcView.kt
@@ -124,4 +124,45 @@ class RotMultiArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RMANode(var i : Int, val state : State = State()) {
+
+        private var next : RMANode? = null
+        private var prev : RMANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RMANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRMANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RMANode {
+            var curr : RMANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
