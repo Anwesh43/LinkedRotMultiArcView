aea3373cfb0419dc5b906da6e24ee0e6d33627cb
Anwesh43 <anweshthecool0@gmail.com>
2021-05-28 17:23:16 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedrotmultiarcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedrotmultiarcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedrotmultiarcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.rotmultiarcview.RotMultiArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotMultiArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
